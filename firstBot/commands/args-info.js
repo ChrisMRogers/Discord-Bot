403574230c37048e5a36afcf66b95f93bb6c97da
Chris Rogers <chrisrogers98@icloud.com>
2021-01-25 18:56:37 -0600

    removed config.json from git

--- firstBot/commands/args-info.js
+++ firstBot/commands/args-info.js
@@ -1,9 +1,9 @@
 module.exports = {
 	name: 'args-info',
-    description: 'Information about the arguments provided.',
-    args: true,
+	description: 'Information about the arguments provided.',
+	args: true,
 	execute(message, args) {
-		if (args[0] === 'foo'){
+		if (args[0] === 'foo') {
 			return message.channel.send('bar');
 		}
 
--- firstBot/commands/roll.js
+++ firstBot/commands/roll.js
@@ -1,24 +1,23 @@
 module.exports = {
-    name:'roll',
-    aliases:['r'],
-    args:true,
-    usage:'[(#dice)d(dice size)]',
-    cooldown:5,
-    execute(message,args){
-        const dice = args[0].split('d');
-        
-        let sumArr = [];
-        for(let i = 0; i<parseInt(dice[0]); i++){
-            sumArr.push(Math.floor(Math.random()*parseInt(dice[1]) + 1));
-        }
-        let response = `${sumArr[0]}`;
-        let sum = parseInt(sumArr[0]);
-        for(let i = 1; i<sumArr.length; i++){
-            response += ` + ${sumArr[i]}`;
-            sum += parseInt(sumArr[i]);
-        }
+	name: 'roll',
+	aliases: ['r'],
+	args: true,
+	usage: '[(#dice)d(dice size)]',
+	cooldown: 5,
+	execute(message, args) {
+		const dice = args[0].split('d');
 
-        message.channel.send(`\`${response} = ${sum}\``);
-        
-    }
-}
+		const sumArr = [];
+		for (let i = 0; i < parseInt(dice[0]); i++) {
+			sumArr.push(Math.floor(Math.random() * parseInt(dice[1]) + 1));
+		}
+		let response = `${sumArr[0]}`;
+		let sum = parseInt(sumArr[0]);
+		for (let i = 1; i < sumArr.length; i++) {
+			response += ` + ${sumArr[i]}`;
+			sum += parseInt(sumArr[i]);
+		}
+
+		message.channel.send(`\`${response} = ${sum}\``);
+	},
+};
