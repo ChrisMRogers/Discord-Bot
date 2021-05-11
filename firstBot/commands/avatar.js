c4d0dee5623764e9d385f6aa3c39ebc82bb468f7
Chris Rogers <chrisrogers98@icloud.com>
2021-05-10 22:32:38 -0500

    Bug fixes

--- firstBot/commands/avatar.js
+++ firstBot/commands/avatar.js
@@ -2,7 +2,17 @@ module.exports = {
 	name: 'avatar',
 	aliases: ['icon', 'pfp'],
 	description: 'Get the avatar URL of the tagged user(s), or your own avatar.',
-	execute(message) {
-		message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: 'png', dynamic: true })}>`);
+	execute(message, args) {
+		if (!args.length) {
+			message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: 'png', dynamic: true })}>`);
+		}
+		else {
+			if (message.mentions.members.first()) {
+				let taggedUser = (message.mentions.users.first());
+				const user = message.guild.member(taggedUser).user
+				message.channel.send(`${taggedUser.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>`);
+			}
+		}
+
 	},
 };
--- firstBot/commands/roll.js
+++ firstBot/commands/roll.js
@@ -7,18 +7,25 @@ module.exports = {
 	cooldown: 5,
 	execute(message, args) {
 		const dice = args[0].split('d');
+		if (dice[0] > 200 || dice[1] > 200) {
+			message.channel.send(`${message.author}, the values you are trying to roll are too large!`)
+		} else if (!Number.isInteger(parseInt(dice[0])) || !Number.isInteger(parseInt(dice[1]))) {
+			message.channel.send(`${message.author}, you probably think you're being funny but all its doing is making me sad that you'd try something so mean.`)
+		} else if (dice[0] < 0 || dice[1] < 0) {
+			message.channel.send(`${message.author}, how in the world do you roll a negative die???`)
+		} else {
+			const sumArr = [];
+			for (let i = 0; i < parseInt(dice[0]); i++) {
+				sumArr.push(Math.floor(Math.random() * parseInt(dice[1]) + 1));
+			}
+			let response = `${sumArr[0]}`;
+			let sum = parseInt(sumArr[0]);
+			for (let i = 1; i < sumArr.length; i++) {
+				response += ` + ${sumArr[i]}`;
+				sum += parseInt(sumArr[i]);
+			}
 
-		const sumArr = [];
-		for (let i = 0; i < parseInt(dice[0]); i++) {
-			sumArr.push(Math.floor(Math.random() * parseInt(dice[1]) + 1));
+			message.channel.send(`\`${response} = ${sum}\``);
 		}
-		let response = `${sumArr[0]}`;
-		let sum = parseInt(sumArr[0]);
-		for (let i = 1; i < sumArr.length; i++) {
-			response += ` + ${sumArr[i]}`;
-			sum += parseInt(sumArr[i]);
-		}
-
-		message.channel.send(`\`${response} = ${sum}\``);
 	},
 };
